b1646ef236ea1da73f02ea1cf7003595c2eeb547
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-10-13 19:22:23 -0300

    initial commit

---
+++ constants/data.js
@@ -0,0 +1,65 @@
+const userData = {
+  githubUsername: "fenriquegimenez",
+  name: "Enrique Giménez",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "enrique@egsoftwarefactory.online",
+  phone: "+595 991 297 525",
+  address: "Asunción - Paraguay.",
+  projects: [
+    {
+      title: "Condel Services",
+      link: "https://condel-services.vercel.app",
+      imgUrl: "/condel.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://egsoftwarefactory.online",
+      imgUrl: "/portfolio.png",
+    },
+  ],
+  about: {
+    title:
+      "Soy un desarrollador de software al que le encanta crear productos y aplicaciones web que impacten en la vida de las personas.",
+    description: [
+      `Soy un apasionado de la tecnología aplicada a resolver problemas cotidianos, que nos permitan invertir nuestro tiempo en cuestiones realmente importantes.`,
+      `Comencé a programar con html, CSS y Javascript, luego fui por el mundo del backend con Python y Java.`,
+      `Pero hoy en día trabajo en lo que realmente me apasiona, el desarrollo web, las aplicaciones web, la tecnología en la nube.`,
+    ],
+    currentProject: "EG Software Factory",
+    currentProjectUrl: "https://egsoftwarefactory.online",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "EG Software Factory",
+      year: "2020",
+      companyLink: "https://egsfotwarefactory.online",
+      desc: "Software development using cutting edge web technologies.",
+    },
+    {
+      title: "Backend Developer",
+      company: "Documenta S.A.",
+      year: "2020",
+      companyLink: "https://documenta.com.py",
+      desc: "API integrations, backend development",
+    },
+    {
+      title: "Technical Back Office",
+      company: "Grupo Luminotecnia",
+      year: "2018",
+      companyLink: "https://grupoluminotecnia.com.py",
+      desc: "Post sales technical support",
+    },
+  ],
+  resumeUrl: "https://linkedin.com/in/fenriquegimenez",
+  socialLinks: {
+    instagram: "https://instagram.com/",
+    twitter: "https://twitter.com/fenriquegvera",
+    linkedin: "https://linkedin.com/in/fenriquegimenez",
+    github: "https://github.com/fenriquegimenez",
+    facebook: "https://facebook.com/",
+  },
+}
+
+export default userData
---
+++ helpers/apihost.js
@@ -0,0 +1,3 @@
+const apiHost = process.env.NEXT_PUBLIC_APIHOST
+
+export default apiHost
