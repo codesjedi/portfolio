f963b21ade1d2a2d07a9066c942bc8c534f05810
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-10-13 19:29:07 -0300

    fixed title

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,35 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import ContainerBlock from "../components/ContainerBlock";
-import FavouriteProjects from "../components/FavouriteProjects";
-import LatestCode from "../components/LatestCode";
-import Hero from "../components/Hero";
-import getLatestRepos from "@lib/getLatestRepos";
-import userData from "@constants/data";
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import ContainerBlock from "../components/ContainerBlock"
+import FavouriteProjects from "../components/FavouriteProjects"
+import LatestCode from "../components/LatestCode"
+import Hero from "../components/Hero"
+import getLatestRepos from "@lib/getLatestRepos"
+import userData from "@constants/data"
 
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="Manu Arora - Developer, Writer, Creator"
-      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+      title="EG Software Factory - Software Developer"
+      description="Web solutions development"
     >
       <Hero />
       <FavouriteProjects />
       <LatestCode repositories={repositories} />
     </ContainerBlock>
-  );
+  )
 }
 
 export const getServerSideProps = async () => {
-  console.log(process.env.GITHUB_AUTH_TOKEN);
-  let token = process.env.GITHUB_AUTH_TOKEN;
+  console.log(process.env.GITHUB_AUTH_TOKEN)
+  let token = process.env.GITHUB_AUTH_TOKEN
 
-  const repositories = await getLatestRepos(userData, token);
+  const repositories = await getLatestRepos(userData, token)
   // console.log("REPOSITORIES", repositories);
 
   return {
     props: {
       repositories,
     },
-  };
-};
+  }
+}
