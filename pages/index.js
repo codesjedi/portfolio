74ae4fd51234fd406bdb5432da7cb9670ad6376f
Enrique Gimenez <enrique@egsoftwarefactory.online>
2022-05-05 18:42:33 -0400

    updated portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import ContainerBlock from "../components/ContainerBlock"
-import FavouriteProjects from "../components/FavouriteProjects"
-import LatestCode from "../components/LatestCode"
-import Hero from "../components/Hero"
-import getLatestRepos from "@lib/getLatestRepos"
-import userData from "@constants/data"
+import ContainerBlock from '../components/ContainerBlock'
+import FavouriteProjects from '../components/FavouriteProjects'
+import LatestCode from '../components/LatestCode'
+import Hero from '../components/Hero'
+import getLatestRepos from '@lib/getLatestRepos'
+import userData from '@constants/data'
 
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="EG Software Factory"
-      description="Web solutions development"
+      title='Enrique Gimenez'
+      description='Web solutions development'
     >
       <Hero />
       <FavouriteProjects />
@@ -22,7 +22,7 @@ export const getServerSideProps = async () => {
   console.log(process.env.GITHUB_AUTH_TOKEN)
   let token = process.env.GITHUB_AUTH_TOKEN
 
-  const repositories = await getLatestRepos(userData, token) || [];
+  const repositories = (await getLatestRepos(userData, token)) || []
   // console.log("REPOSITORIES", repositories);
 
   return {
